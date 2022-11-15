a51fa51cd1a1874e66caaadae87b0ae356d19dc2
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2022-11-16 01:07:11 +0200

    Add files via upload

---
+++ components/NotesList.jsx
@@ -0,0 +1,35 @@
+
+
+import { useState } from 'react';
+
+function NotesList({noteText,noteIndex,deleteHandler}){   
+  console.log(noteIndex)                     //// LIST COMPONENT
+  const [modal,setModal] = useState(false);
+
+  const toggleModal = () =>{
+    setModal(!modal)
+  }
+
+  return(  /// JSX RETURN ELEMENTS RESULTS
+    <div className='container'> 
+                                      
+    <span onClick={toggleModal} className='element'><h4 className='elementtitle'>NOTE TITLE</h4>
+    {modal &&(
+    <div className='modal'>
+      <div onClick={toggleModal} className='overlay'>
+        <div className='modal-content'>
+          <span className='elementmodal'><h4 className='titlemodal'>NOTE TITLE</h4>
+          {noteText}<button onClick={toggleModal} className='closebutton'>x</button></span>
+        </div>
+      </div>
+    </div>
+      
+    
+    )}<span style={{fontSize:'14px',margin:'4px'}}>{noteText}</span><button onClick={() => deleteHandler(noteText,noteIndex)} className='closebutton'>x</button>
+    
+    </span>
+    </div>
+    )
+}
+
+export default NotesList
