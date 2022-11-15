69cff081e53a12e33c424a5b722b8d7c80a9995d
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2022-11-16 01:35:30 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import AddNotesComponent from './components/NotesAdd';
+
+
+
+function App() {                                                        //// RENDER ROOT COMPONENT
+  return (
+   <>
+   <div className='fullpagediv'>
+    <AddNotesComponent />
+    
+   </div>
+   </>
+  );
+}
+
+export default App;
