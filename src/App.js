be8d839bb7512043d52578ee5ae8f21480f36e97
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2022-11-24 00:03:49 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 
 import './App.css';
 import AddNotesComponent from './components/NotesAdd';
+import React, { createContext, useState } from 'react';
 
 
+export const varContext = createContext()
+export const var2Context = createContext()
+export const resultContext = createContext()
 
-function App() {                                                        //// RENDER ROOT COMPONENT
+
+function App() {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState(false);
+  const [result, setResult] = useState([]);
   return (
-   <>
-   <div className='fullpagediv'>
-    <AddNotesComponent />
-    
-   </div>
-   </>
+    <>
+      <div className='fullpagediv'>
+        <resultContext.Provider value={{ result, setResult }}>
+          <varContext.Provider value={{ title, setTitle }}>
+            <var2Context.Provider value={{ text, setText }}>
+
+              <AddNotesComponent />
+              
+            </var2Context.Provider>
+          </varContext.Provider>
+        </resultContext.Provider>
+      </div>
+    </>
   );
 }
 
